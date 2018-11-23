da3ba2b6bff6d7525af57453200928d9306015f3
David Soares <davidsou@hotmail.com>
2018-11-23 21:18:27 -0200

    aula 10

--- treinamentoangular/src/app/app.module.ts
+++ treinamentoangular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelSimplesComponent
+    PainelSimplesComponent,
+    BarraNavegacaoComponent,
+    MenuLateralComponent,
+
   ],
   imports: [
     BrowserModule,
